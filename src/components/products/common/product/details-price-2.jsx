640f141fe266e7027cdb3789adbf4adeda592d36
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-02-15 23:41:57 -0800

    changed rating from modal to single product page

--- src/components/products/common/product/details-price-2.jsx
+++ src/components/products/common/product/details-price-2.jsx
@@ -6,6 +6,7 @@ import StarRatings from 'react-star-ratings';
 import RatingModal from "../../../modal/RatingModal";
 import {useSelector} from "react-redux";
 import { StarOutlined } from "@ant-design/icons";
+import {toast} from "react-toastify";
 
 const DetailsWithPrice2 = ({symbol, item, addToCartClicked, BuynowClicked, addToWishlistClicked, product}) => {
     const [open, isOpen] = useState(false);
@@ -74,140 +75,7 @@ const DetailsWithPrice2 = ({symbol, item, addToCartClicked, BuynowClicked, addTo
                         <h3>${product.price}</h3>
 
                     </div>
-                    <div className="border-product">
-                        {/*<h6 className="product-title">Rating</h6>*/}
-                        {/*<RatingModal/>*/}
-                        {user ? <StarRatings
-                            name={product._id}
-                            numberOfStars={5}
-                            rating={2}
-                            changeRating={(newRating, name) => console.log('newRating', newRating, 'name', name)}
-                            isSelectable={true}
-                            starRatedColor='#79868F'
-                        />  :
-                            <>
-                                <Link to={`${process.env.PUBLIC_URL}/pages/login`}>
-                                    <StarOutlined className="text-danger" />
-                                    <br />{" "}
-                                    Login to leave rating
-                                </Link>
 
-                            </>
-                        }
-                        {/*<div className="product-icon" style={{display: 'block'}}>*/}
-                        {/*    <ul className="product-social" style={{display: 'none'}}>*/}
-                        {/*        <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook"></i></a></li>*/}
-                        {/*        <li><a href="https://plus.google.com/discover" target="_blank"><i className="fa fa-google-plus"></i></a></li>*/}
-                        {/*        <li><a href="https://twitter.com/" target="_blank"><i className="fa fa-twitter"></i></a></li>*/}
-                        {/*        <li><a href="https://www.instagram.com/" target="_blank"><i className="fa fa-instagram"></i></a></li>*/}
-                        {/*    </ul>*/}
-                        {/*    <button className="wishlist-btn" onClick={() => addToWishlistClicked(item)}><i*/}
-                        {/*        className="fa fa-heart" style={{borderLeft: 'none'}}></i><span*/}
-                        {/*        className="title-font">Add To WishList</span>*/}
-                        {/*    </button>*/}
-                        {/*</div>*/}
-                    </div>
-                    <div className="border-product">
-                        {/*<h3></h3>*/}
-
-                    </div>
-                    {/*{item.variants?*/}
-                    {/*<ul >*/}
-                    {/*    <Slider {...colorsnav} asNavFor={this.props.navOne} ref={slider => (this.slider1 = slider)} className="color-variant">*/}
-                    {/*        {item.variants.map((vari, i) => {*/}
-                    {/*            return <li className={vari.color} key={i} title={vari.color}></li>*/}
-                    {/*        })}*/}
-                    {/*    </Slider>*/}
-                    {/*</ul>:''*/}
-                    {/*}*/}
-                    {/*<div className="border-product">*/}
-                    {/*    <span className="instock-cls">{stock}</span>*/}
-                    {/*</div>*/}
-                    <div className="product-description border-product">
-                        {/*{item.size?*/}
-                        <div>
-                            {/*<h6 className="product-title size-text">select size*/}
-                            {/*    <span><a href="#" data-toggle="modal"*/}
-                            {/*             data-target="#sizemodal" onClick={onOpenModal} >size chart</a>*/}
-                            {/*    </span>*/}
-                            {/*</h6>*/}
-                            {/*<div className="modal fade" id="sizemodal" tabIndex="-1"*/}
-                            {/*     role="dialog" aria-labelledby="exampleModalLabel"*/}
-                            {/*     aria-hidden="true">*/}
-                            {/*    <div className="modal-dialog modal-dialog-centered"*/}
-                            {/*         role="document">*/}
-                            {/*        <div className="modal-content">*/}
-                            {/*            <div className="modal-header">*/}
-                            {/*                <h5 className="modal-title"*/}
-                            {/*                    id="exampleModalLabel">Sheer Straight*/}
-                            {/*                    Kurta</h5>*/}
-                            {/*                <button type="button" className="close"*/}
-                            {/*                        data-dismiss="modal" aria-label="Close">*/}
-                            {/*                    <span aria-hidden="true">&times;</span>*/}
-                            {/*                </button>*/}
-                            {/*            </div>*/}
-                            {/*            <div className="modal-body">*/}
-                            {/*                <img src={`${process.env.PUBLIC_URL}/assets/images/size-chart.jpg`} alt="" className="img-fluid"/>*/}
-                            {/*            </div>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*</div>*/}
-                            <div className="size-box">
-                                {/*<i className="fa fa-heart-o" aria-hidden="true"></i>*/}
-                                <ul>
-                                    {/*{item.size.map((size, i) => {*/}
-                                    {/*    return <li key={i}><a href="#">{size}</a></li>*/}
-                                    {/*})}*/}
-
-                                </ul>
-                            </div>
-                        </div>
-                        {/*:''}*/}
-
-
-
-                        {/*<span className="instock-cls">{stock}</span>*/}
-                        {/*<h6 className="product-title">quantity</h6>*/}
-                        {/*<div className="qty-box" style={{display: 'inline-block'}}>*/}
-                        {/*    <div className="input-group">*/}
-                        {/*          <span className="input-group-prepend">*/}
-                        {/*            <button type="button" className="btn quantity-left-minus" onClick={minusQty} data-type="minus" data-field="">*/}
-                        {/*             <i className="fa fa-angle-left"></i>*/}
-                        {/*            </button>*/}
-                        {/*          </span>*/}
-                        {/*        <input type="text" name="quantity" value={quantity} onChange={changeQty} className="form-control input-number" />*/}
-                        {/*        <span className="input-group-prepend">*/}
-                        {/*        <button type="button" className="btn quantity-right-plus" onClick={plusQty} data-type="plus" data-field="">*/}
-                        {/*        <i className="fa fa-angle-right"></i>*/}
-                        {/*        </button>*/}
-                        {/*       </span>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
-
-
-
-
-
-
-
-
-
-                    </div>
-                    <div className="product-buttons" >
-                        <a className="btn btn-solid" onClick={() => addToCartClicked(item, quantity)}>add to cart</a>
-                        {/*<Link to={`${process.env.PUBLIC_URL}/checkout`} className="btn btn-solid" onClick={() => BuynowClicked(item, this.state.quantity)} >*/}
-                        {/*    buy now*/}
-                        {/*</Link>*/}
-                    </div>
-                    {/*<div className="border-product">*/}
-                        {/*<h6 className="product-title">product details</h6>*/}
-                        {/*/!*<p>{item.shortDetails}</p>*!/*/}
-                        {/*{product.description}*/}
-
-                        {/*<Link to={`${process.env.PUBLIC_URL}/checkout`} className="btn btn-solid" onClick={() => BuynowClicked(item, quantity)} >*/}
-                        {/*    buy now*/}
-                        {/*</Link>*/}
-                    {/*</div>*/}
 
                     <div className="border-product">
                         <h6 className="product-title">favorite</h6>
@@ -227,6 +95,94 @@ const DetailsWithPrice2 = ({symbol, item, addToCartClicked, BuynowClicked, addTo
 
 
 
+
+                    <div className="border-product" style={{paddingTop: '20px'}}>
+                        {/*{item.size?*/}
+
+
+                        <div className="product-buttons" style={{marginBottom: '0'}}>
+                            <a className="btn btn-solid" onClick={() => addToCartClicked(item, quantity)}>add to cart</a>
+                            {/*<Link to={`${process.env.PUBLIC_URL}/checkout`} className="btn btn-solid" onClick={() => BuynowClicked(item, this.state.quantity)} >*/}
+                            {/*    buy now*/}
+                            {/*</Link>*/}
+                        </div>
+
+
+
+
+
+                    </div>
+
+
+
+
+
+
+                    {/*<div className="border-product">*/}
+                        {/*<h6 className="product-title">product details</h6>*/}
+                        {/*/!*<p>{item.shortDetails}</p>*!/*/}
+                        {/*{product.description}*/}
+
+                        {/*<Link to={`${process.env.PUBLIC_URL}/checkout`} className="btn btn-solid" onClick={() => BuynowClicked(item, quantity)} >*/}
+                        {/*    buy now*/}
+                        {/*</Link>*/}
+                    {/*</div>*/}
+
+
+
+
+
+
+
+
+
+
+
+
+                    <div className="border-product">
+                        {/*<h6 className="product-title">Rating</h6>*/}
+                        {/*<RatingModal/>*/}
+
+                        {user ?
+                            <>
+                                <StarRatings
+                                    name={product._id}
+                                    numberOfStars={5}
+                                    rating={2}
+                                    changeRating={(newRating, name) => console.log('newRating', newRating, 'name', name)}
+                                    isSelectable={true}
+                                    starRatedColor='#79868F'
+                                />
+                                {/*<a className="btn btn-solid" >leave rating</a>*/}
+
+
+
+                                <div className="product-icon" style={{display: 'block', marginTop: '20px'}}>
+                                    <button className="wishlist-btn" onClick={() => {toast.success("Thanks for your review. It will apper soon")}}><i
+                                        className="fa fa-star" style={{borderLeft: 'none'}}></i><span
+                                        className="title-font">Leave Rating</span>
+                                    </button>
+                                </div>
+                            </>
+                            :
+                            <>
+                                <Link to={`${process.env.PUBLIC_URL}/pages/login`}>
+                                    <StarOutlined className="text-danger" />
+                                    <br />{" "}
+                                    Login to leave rating
+                                </Link>
+
+                            </>
+                        }
+
+
+                    </div>
+
+
+
+
+
+
                     <div className="border-product">
                         <h6 className="product-title">share it</h6>
                         <div className="product-icon" style={{display: 'block'}}>
@@ -242,6 +198,9 @@ const DetailsWithPrice2 = ({symbol, item, addToCartClicked, BuynowClicked, addTo
                             </button>
                         </div>
                     </div>
+
+
+
 
                     <div className="border-product">
                         <h6 className="product-title">Time Reminder</h6>
