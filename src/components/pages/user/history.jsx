1ebde200a3f9d7c4332b47928386fd1cf3fe924b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-02-28 01:18:19 -0800

    added purchase history in the user dashboard

--- src/components/pages/user/history.jsx
+++ src/components/pages/user/history.jsx
@@ -3,6 +3,7 @@ import Breadcrumb from "../../common/breadcrumb";
 import UserNav from "../../nav/UserNav";
 import {Link} from "react-router-dom";
 import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
+import ShowPaymentInfo from "../../../components/cards/ShowPaymentInfo";
 import { getUserOrders } from "../../../functions/user";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -19,6 +20,75 @@ const UserHistory = (props) => {
             console.log(JSON.stringify(res.data, null, 4));
             setOrders(res.data);
         });
+
+
+    const showOrderInTable = (order) => (
+        <table className="table table-bordered">
+            <thead className="thead-light">
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Color</th>
+                <th scope="col">Count</th>
+                <th scope="col">Shipping</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            {order.products.map((p, i) => (
+                <tr key={i}>
+                    <td>
+                        <b>{p.product.title}</b>
+                    </td>
+                    <td>{p.product.price}</td>
+                    <td>{p.product.brand}</td>
+                    <td>{p.color}</td>
+                    <td>{p.count}</td>
+                    <td>
+                        {p.product.shipping === "Yes" ? (
+                            <CheckCircleOutlined style={{ color: "green" }} />
+                        ) : (
+                            <CloseCircleOutlined style={{ color: "red" }} />
+                        )}
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+
+
+    const showEachOrders = () =>
+        orders.map((order, i) => (
+            <div key={i} className="m-5 p-3 card">
+                <ShowPaymentInfo order={order} />
+                {showOrderInTable(order)}
+                <div className="row">
+                    <div className="col">
+                        <p>PDF download</p>
+                    </div>
+                </div>
+            </div>
+        ));
+
+    const userHistoryForm = () => (
+        <div className="row">
+            <div className="col-sm-12">
+                <div className="box">
+                    <div style={{marginTop: '30px'}} className="box-title">
+                        <h3 style={{fontWeight: '600'}}>User Purchase Orders</h3>
+                        {/*<a href="#">Edit</a>*/}
+                    </div>
+                    <div className="box-content">
+                        {showEachOrders()}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+
 
     return (
         <div>
@@ -61,76 +131,100 @@ const UserHistory = (props) => {
                         </div>
                         <div className="col-lg-9">
                             <div className="dashboard-right">
-                                <div className="dashboard">
-                                    <div className="page-title">
-                                        <h2>My Dashboard</h2>
+                                <div style={{textAlign: 'center'}} className="dashboard">
+
+                                    <div  className="page-title">
+                                        <h2 style={{fontWeight: "800"}}>History</h2>
                                     </div>
-                                    <div className="welcome-msg">
-                                        <p>Hello, MARK JECNO !</p>
-                                        <p>From your My Account Dashboard you have the ability to view a snapshot of
-                                            your recent account activity and update your account information. Select
-                                            a link below to view or edit information.</p>
-                                    </div>
+
+
                                     <div className="box-account box-info">
+
                                         <div className="box-head">
-                                            <h2>Account Information</h2>
+                                            <h4>Account Information</h4>
                                         </div>
-                                        <div className="row">
-                                            <div className="col-sm-6">
-                                                <div className="box">
-                                                    <div className="box-title">
-                                                        <h3>Contact Information</h3>
-                                                        <a href="#">Edit</a>
-                                                    </div>
-                                                    <div className="box-content">
-                                                        <h6>MARK JECNO</h6>
-                                                        <h6>MARk-JECNO@gmail.com</h6>
-                                                        <h6><a href="#">Change Password</a></h6>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-sm-6">
-                                                <div className="box">
-                                                    <div className="box-title">
-                                                        <h3>Newsletters</h3>
-                                                        <a href="#">Edit</a>
-                                                    </div>
-                                                    <div className="box-content">
-                                                        <p>
-                                                            You are currently not subscribed to any newsletter.
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <div className="box">
-                                                <div className="box-title">
-                                                    <h3>Address Book</h3>
-                                                    <a href="#">Manage Addresses</a>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col-sm-6">
-                                                        <h6>Default Billing Address</h6>
-                                                        <address>
-                                                            You have not set a default billing address.<br/>
-                                                            <a href="#">Edit Address</a>
-                                                        </address>
-                                                    </div>
-                                                    <div className="col-sm-6">
-                                                        <h6>Default Shipping Address</h6>
-                                                        <address>
-                                                            You have not set a default shipping address.<br />
-                                                            <a href="#">Edit Address</a>
-                                                        </address>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        {userHistoryForm()}
+
+
+
+
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        {/*<div className="col-lg-9">*/}
+                        {/*    <div className="dashboard-right">*/}
+                        {/*        <div className="dashboard">*/}
+                        {/*            <div className="page-title">*/}
+                        {/*                <h2>My Dashboard</h2>*/}
+                        {/*            </div>*/}
+                        {/*            {orders.length > 0 ? "user purchase orders" : 'no purchase orders'}*/}
+                        {/*            <div className="welcome-msg">*/}
+                        {/*                <p>Hello, MARK JECNO !</p>*/}
+                        {/*                <p>From your My Account Dashboard you have the ability to view a snapshot of*/}
+                        {/*                    your recent account activity and update your account information. Select*/}
+                        {/*                    a link below to view or edit information.</p>*/}
+                        {/*            </div>*/}
+                        {/*            <div className="box-account box-info">*/}
+                        {/*                <div className="box-head">*/}
+                        {/*                    <h2>Account Information</h2>*/}
+                        {/*                </div>*/}
+                        {/*                <div className="row">*/}
+                        {/*                    <div className="col-sm-6">*/}
+                        {/*                        <div className="box">*/}
+                        {/*                            <div className="box-title">*/}
+                        {/*                                <h3>Contact Information</h3>*/}
+                        {/*                                <a href="#">Edit</a>*/}
+                        {/*                            </div>*/}
+                        {/*                            <div className="box-content">*/}
+                        {/*                                <h6>MARK JECNO</h6>*/}
+                        {/*                                <h6>MARk-JECNO@gmail.com</h6>*/}
+                        {/*                                <h6><a href="#">Change Password</a></h6>*/}
+                        {/*                            </div>*/}
+                        {/*                        </div>*/}
+                        {/*                    </div>*/}
+                        {/*                    <div className="col-sm-6">*/}
+                        {/*                        <div className="box">*/}
+                        {/*                            <div className="box-title">*/}
+                        {/*                                <h3>Newsletters</h3>*/}
+                        {/*                                <a href="#">Edit</a>*/}
+                        {/*                            </div>*/}
+                        {/*                            <div className="box-content">*/}
+                        {/*                                <p>*/}
+                        {/*                                    You are currently not subscribed to any newsletter.*/}
+                        {/*                                </p>*/}
+                        {/*                            </div>*/}
+                        {/*                        </div>*/}
+                        {/*                    </div>*/}
+                        {/*                </div>*/}
+                        {/*                <div>*/}
+                        {/*                    <div className="box">*/}
+                        {/*                        <div className="box-title">*/}
+                        {/*                            <h3>Address Book</h3>*/}
+                        {/*                            <a href="#">Manage Addresses</a>*/}
+                        {/*                        </div>*/}
+                        {/*                        <div className="row">*/}
+                        {/*                            <div className="col-sm-6">*/}
+                        {/*                                <h6>Default Billing Address</h6>*/}
+                        {/*                                <address>*/}
+                        {/*                                    You have not set a default billing address.<br/>*/}
+                        {/*                                    <a href="#">Edit Address</a>*/}
+                        {/*                                </address>*/}
+                        {/*                            </div>*/}
+                        {/*                            <div className="col-sm-6">*/}
+                        {/*                                <h6>Default Shipping Address</h6>*/}
+                        {/*                                <address>*/}
+                        {/*                                    You have not set a default shipping address.<br />*/}
+                        {/*                                    <a href="#">Edit Address</a>*/}
+                        {/*                                </address>*/}
+                        {/*                            </div>*/}
+                        {/*                        </div>*/}
+                        {/*                    </div>*/}
+                        {/*                </div>*/}
+                        {/*            </div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
             </section>
